99296c3d835a26954356015bba121792e26dbcea
Hamza Ali <hamza.ali@decathlon.com>
2018-01-11 15:55:01 +0100

    fixing checkbox issue

--- 06_react_redux/02_dynamism_in_web/03_todo_app/src/AddTask.js
+++ 06_react_redux/02_dynamism_in_web/03_todo_app/src/AddTask.js
@@ -5,7 +5,7 @@ import Delete from './Delete'
 function AddTask(props){
   return (
     <div key={Math.random()}>
-      <p>{props.myTask.name} <input type="checkbox" onChange={()=>{props.isCheck(props.myTask)}}/></p>
+      <p>{props.myTask.name} <input type="checkbox" checked={props.myTask.checked} onChange={()=>{props.isCheck(props.myTask)}}/></p>
       <Delete Checked={props.myTask.checked} handleDelete={props.handleDelete} myTask={props.myTask}/>
     </div>
   )
