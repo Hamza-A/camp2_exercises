286d9c8db58b7178bd521c0d4bce3e1a8ea972c7
Hamza Ali <hamza.ali@decathlon.com>
2018-02-24 18:32:46 +0100

    exercice memory

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,26 @@
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+
+function check(firstInput, secondInput){
+  const firstCard = cards[firstInput];
+  const secondCard = cards[secondInput];
+
+  if (firstCard === secondCard){
+    console.log(firstCard + "  " + secondCard);
+    console.log("its a match!");
+  } else {
+    console.log(firstCard + "  " + secondCard);
+    console.log("try again!");
+  }
+  reader.close();
+}
+
+check(0, 3);
