70162bc0b467223ec5855fd9f3fbd3999376349f
Hamza Ali <hamza.ali@decathlon.com>
2018-02-26 19:19:06 +0100

    completing this exercices

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -4,28 +4,28 @@ const car = {
   distance: 0,
 
   start: function(){
-      this.speed = 0
-      this.distance = 0
-      return this;
-    },
+    this.speed = 0;
+    this.distance = 0;
+    return this;
+  },
 
   changeSpeed: function(newSpeed){
-      this.speed = newSpeed;
+    this.speed = newSpeed;
     return this;
-    },
+  },
 
   drive: function(drivenMinutes){
-     this.distance = this.distance + ((drivenMinutes/60) * this.speed);
-     return this;
+    this.distance = this.distance + ((drivenMinutes/60) * this.speed);
+    return this;
 
-   },
+  },
   showDistance: function(){
 
     console.log(this.distance + " Km");
     return this;
-    }
+  }
 
-}
+};
 
 car.start().changeSpeed(130).drive(42).showDistance();
 
--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -1,3 +1,55 @@
 // See Sparta courses for the exercise summary
+const orangeTree = {
+  height : 0,
+  age : 0,
+  oranges : 0,
+  alive: false,
 
+  pickAnOrange: function(){
+    if (this.oranges >= 1){
+      this.oranges -= 1;
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  ageOneYear: function(){
+    if (this.age <= 100){
+      this.age ++;
+    }
+
+    if (this.age < 10){
+      this.height = this.height + 25;
+    } else if (this.age >= 10 && this.age < 20) {
+      this.height += 10;
+    } else if (this.age >= 20 && this.age < 100){
+      this.height = this.height;
+    } else if (this.age >= 100){
+      this.alive = false;
+    }
+
+    if (this.age >= 5 && this.age < 10){
+      this.oranges = 10;
+    } else if (this.age >= 10 && this.age < 20){
+      this.oranges = 20;
+    } else if (this.age >= 20 && this.age < 40){
+      this.oranges = 5;
+    } else {
+      this.oranges = 0;
+    }
+    return this;
+  },
+
+  seed: function(){
+    this.height = 0;
+    this.age = 0;
+    this.oranges = 0;
+    this.alive = true;
+    return this;
+  }
+};
+
+orangeTree.ageOneYear();
+console.log(orangeTree);
 module.exports = orangeTree;
