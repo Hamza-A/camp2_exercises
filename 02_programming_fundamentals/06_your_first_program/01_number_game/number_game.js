ebaf99f62b52471bcce785b0c8d1494433efcce6
Hamza Ali <hamza.ali@decathlon.com>
2018-02-26 16:35:50 +0100

    trying to do number games from scratch

--- 02_programming_fundamentals/06_your_first_program/01_number_game/number_game.js
+++ 02_programming_fundamentals/06_your_first_program/01_number_game/number_game.js
@@ -5,32 +5,27 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
-function getRandomArbitrary(min, max) {
-  return parseInt(Math.random() * (max - min) + min,10);
-}
+const rightNumber = (Math.round(Math.random() * 100));
 
-let numberToSearch = getRandomArbitrary(0,100);
+console.log(rightNumber);
 
-console.log(numberToSearch);
-
-function check(userInput){
+function tryToGuessRightNumber(userInput){
   let message;
-  let userInput2=parseInt(userInput,10);
-
-  if (userInput2 === numberToSearch){
+  let parsedInput = parseInt(userInput, 10);
+  if (parsedInput < 1 || parsedInput > 100) {
+    message = "Out of range, please choose a number between 1 and 100 \n";
+  } else if (parsedInput > rightNumber) {
+    message = "tow high, try again \n";
+  } else if (parsedInput === rightNumber) {
     console.log("You won!");
     reader.close();
-    return ;
-  } else if (userInput2 > numberToSearch && userInput2 <= 100){
-    message= "Too high \n" ;
-  } else if (userInput2 < numberToSearch && userInput2 >= 1){
-    message="Too low \n";
-  }else if (userInput2 < 1 || userInput2 > 100){
-    message="The number is between 1 and 100 \n";
-  }else {
-    message="This was not a number \n";
+    return;
+  } else if (parsedInput < rightNumber){
+    message = "tow low, try again \n";
+  } else {
+    message = "please type a number \n";
   }
-  reader.question(message, check);
+  reader.question(message, tryToGuessRightNumber);
 }
 
-reader.question("Trouve le nombre entre 1 et 100 ", check);
+reader.question("Try to guess the right number: \n", tryToGuessRightNumber);
