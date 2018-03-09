bd917d96f5e5ded7373ca92e91b5ec02a9ce205b
Hamza Ali <hamza.ali@decathlon.com>
2018-03-09 17:07:41 +0100

    completing pyramid exercice

--- 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
+++ 02_programming_fundamentals/03_algorithms_day_1/05_play_with_for_loop_bonus/05_pyramid.js
@@ -6,3 +6,16 @@
 //  *****
 // *******
 // ```
+
+let stars = ["*"];
+let space = [" ", " ", " ",];
+let result = space.join("") + stars.join("");
+
+console.log(result);
+
+for (let i = 1; i < 4; i++){
+  stars.push("*", "*");
+  space.pop();
+  let result = space.join("") + stars.join("");
+  console.log(result);
+}
